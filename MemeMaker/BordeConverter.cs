ffceefc328eb44c2987e5719b8bdd9f822e81087
daaronb <aaron.diaz@iesdoctorbalmis.com>
2022-10-27 20:46:38 +0200

    finalizado

--- MemeMaker/BordeConverter.cs
+++ MemeMaker/BordeConverter.cs
@@ -12,7 +12,13 @@ namespace MemeMaker
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            if ((Boolean)value){
+                return "3";
+            }
+            else
+            {
+                return "0";
+            }
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
